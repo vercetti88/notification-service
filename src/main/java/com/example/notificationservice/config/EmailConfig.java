7adc802ec2079a7f99d4303b23538c2a801e0874
vercetti88 <125506451+vercetti88@users.noreply.github.com>
2023-02-17 01:06:34 +0600

    Update EmailConfig.java

--- src/main/java/com/example/notificationservice/config/EmailConfig.java
+++ src/main/java/com/example/notificationservice/config/EmailConfig.java
@@ -15,8 +15,8 @@ public class EmailConfig {
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
 
-        mailSender.setUsername("m.46.band.user@gmail.com"); //TODO создать и добавить почту gmail,
-        mailSender.setPassword("lnlugnrugyyhufim"); //TODO создать и добавить пароль для приложения по ссылке https://support.google.com/accounts/answer/185833?hl=ru
+        mailSender.setUsername("m.46.band.user@gmail.com"); 
+        mailSender.setPassword(">>"); 
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
