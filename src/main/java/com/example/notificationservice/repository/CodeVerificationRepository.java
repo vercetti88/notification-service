7445a38d2626f0e05aad638e22166c02350ac4ad
Abdulaziz Baisarov <quntxm@gmail.com>
2023-02-17 22:49:50 +0600

    some changes

--- src/main/java/com/example/notificationservice/repository/CodeVerificationRepository.java
+++ src/main/java/com/example/notificationservice/repository/CodeVerificationRepository.java
@@ -7,9 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface CodeVerificationRepository extends JpaRepository<CodeVerificationEntity, Long> {
 
-    //TODO создать метод который возвращает CodeVerificationEntity по email с помощью ключевых слов JPA
     public CodeVerificationEntity findByEmail(String email);
 
-    //TODO создать void метод который удаляет запись по email с помощью ключевых слов JPA
     public void deleteByEmail(String email);
 }
